2347cff696ce7f5999a0a30d6939422545be22a0
mfa188 <prakrut.rajesh07@gmail.com>
2024-02-07 13:42:30 +0100

    Curated list of openrouter models

--- lib/models/fetch-models.ts
+++ lib/models/fetch-models.ts
@@ -89,6 +89,44 @@ export const fetchOpenRouterModels = async () => {
 
     const { data } = await response.json()
 
+    // Define your curated list of model IDs or names
+    const curatedModelList = ['nousresearch/nous-capybara-7b:free', 'mistralai/mistral-7b-instruct:free', 'gryphe/mythomist-7b:free', 'undi95/toppy-m-7b:free', 'openrouter/cinematika-7b:free', 'rwkv/rwkv-5-world-3b', 'recursal/rwkv-5-3b-ai-town', 'recursal/eagle-7b', 'huggingfaceh4/zephyr-7b-beta:free',  'openchat/openchat-7b:free'];
+
+    const openRouterModels = data
+      .filter((model: { id: string }) => curatedModelList.includes(model.id))
+      .map(
+        (model: {
+          id: string
+          name: string
+          context_length: number
+        }): OpenRouterLLM => ({
+          modelId: model.id as LLMID,
+          modelName: model.id,
+          provider: "openrouter",
+          hostedId: model.name,
+          platformLink: "https://openrouter.dev",
+          imageInput: false,
+          maxContext: model.context_length
+        })
+      )
+
+    return openRouterModels
+  } catch (error) {
+    console.error("Error fetching Open Router models: " + error)
+    toast.error("Error fetching Open Router models: " + error)
+  }
+}
+
+/*export const fetchOpenRouterModels = async () => {
+  try {
+    const response = await fetch("https://openrouter.ai/api/v1/models")
+
+    if (!response.ok) {
+      throw new Error(`OpenRouter server is not responding.`)
+    }
+
+    const { data } = await response.json()
+
     const openRouterModels = data.map(
       (model: {
         id: string
@@ -110,4 +148,4 @@ export const fetchOpenRouterModels = async () => {
     console.error("Error fetching Open Router models: " + error)
     toast.error("Error fetching Open Router models: " + error)
   }
-}
+}*/
