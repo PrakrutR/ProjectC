712a6497ef15f4714bdf3edfbb38e00b700cbe7b
mfa188 <prakrut.rajesh07@gmail.com>
2024-02-12 00:14:10 +0100

    Revert "Changed gemini hopefully"
    This reverts commit 108b1ec0efdf69ee3cbda082014ee249b1a86722.

--- app/api/chat/google/route.ts
+++ app/api/chat/google/route.ts
@@ -23,6 +23,10 @@ export async function POST(request: Request) {
       model: chatSettings.model,
       safetySettings: [
         {
+          category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
           category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
           threshold: HarmBlockThreshold.BLOCK_NONE
         },
