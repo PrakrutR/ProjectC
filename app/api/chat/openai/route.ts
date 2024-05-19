862d01fbcaa50f437b14d0e36138dd9afdef0ed9
mfa188 <prakrut.rajesh07@gmail.com>
2024-05-19 06:45:06 +0200

    roolback

--- app/api/chat/openai/route.ts
+++ app/api/chat/openai/route.ts
@@ -3,41 +3,9 @@ import { ChatSettings } from "@/types"
 import { OpenAIStream, StreamingTextResponse } from "ai"
 import { ServerRuntime } from "next"
 import OpenAI from "openai"
-import { createHeaders, PORTKEY_GATEWAY_URL } from "portkey-ai"
+import { ChatCompletionCreateParamsBase } from "openai/resources/chat/completions.mjs"
 
 export const runtime: ServerRuntime = "edge"
-
-const PORTKEY_API_KEY = process.env.PORTKEY_API_KEY // Import API key from environment variable
-const PORTKEY_CONFIG_ID = "pc-defalu-91be34"
-
-async function getFromPortkeyCache(key: string) {
-  const response = await fetch(
-    `${PORTKEY_GATEWAY_URL}/cache/${encodeURIComponent(key)}`,
-    {
-      headers: {
-        Authorization: `Bearer ${PORTKEY_API_KEY}`,
-        "Content-Type": "application/json"
-      }
-    }
-  )
-
-  if (response.ok) {
-    return response.json()
-  }
-
-  return null
-}
-
-async function setToPortkeyCache(key: string, data: any) {
-  await fetch(`${PORTKEY_GATEWAY_URL}/cache`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${PORTKEY_API_KEY}`,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ key, data })
-  })
-}
 
 export async function POST(request: Request) {
   const json = await request.json()
@@ -48,15 +16,8 @@ export async function POST(request: Request) {
 
   try {
     const profile = await getServerProfile()
+
     checkApiKey(profile.openai_api_key, "OpenAI")
-
-    const cacheKey = JSON.stringify({ messages }) // Use only messages for cache key
-
-    // Check Portkey cache first
-    const cachedResponse = await getFromPortkeyCache(cacheKey)
-    if (cachedResponse) {
-      return new Response(JSON.stringify(cachedResponse), { status: 200 })
-    }
 
     const openai = new OpenAI({
       apiKey: profile.openai_api_key || "",
@@ -73,15 +34,11 @@ export async function POST(request: Request) {
         chatSettings.model === "gpt-4-vision-preview" ||
         chatSettings.model === "gpt-4o"
           ? 4096
-          : null,
+          : null, // TODO: Fix
       stream: true
     })
 
     const stream = OpenAIStream(response)
-
-    // Cache the response in Portkey
-    const responseBody = await response.json()
-    await setToPortkeyCache(cacheKey, responseBody)
 
     return new StreamingTextResponse(stream)
   } catch (error: any) {
