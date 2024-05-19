8331532353f5fe7713021317928cae6386af22e0
mfa188 <prakrut.rajesh07@gmail.com>
2024-05-19 14:43:11 +0200

    Adding gooseai final version

--- app/api/chat/gooseai/route.ts
+++ app/api/chat/gooseai/route.ts
@@ -1,5 +1,7 @@
+import { CHAT_SETTING_LIMITS } from "@/lib/chat-setting-limits"
 import { checkApiKey, getServerProfile } from "@/lib/server/server-chat-helpers"
 import { ChatSettings } from "@/types"
+import { max } from "date-fns"
 import { Configuration, OpenAIApi } from "openai1"
 
 export async function POST(request: Request) {
@@ -18,11 +20,16 @@ export async function POST(request: Request) {
       basePath: "https://api.goose.ai/v1"
     })
 
+    const prompt = messages.map(message => message.content).join("\n")
+    const max_tokens =
+      CHAT_SETTING_LIMITS[chatSettings.model].MAX_CONTEXT_LENGTH - prompt.length
+
     const gooseAI = new OpenAIApi(configuration)
 
     const response = await gooseAI.createCompletion(chatSettings.model, {
-      prompt: messages.map(message => message.content).join("\n"),
-      temperature: chatSettings.temperature
+      prompt: prompt,
+      temperature: chatSettings.temperature,
+      max_tokens: max_tokens
     })
 
     const completionText = response.data.choices
